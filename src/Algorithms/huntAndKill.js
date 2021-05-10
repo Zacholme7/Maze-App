1d7f422e7443be572adbdbc1e6006b60fcd0b487
Zac <zacholme@gmail.com>
2021-05-10 13:33:57 -0500

    rewrote hunt and kill, previous algo allowed it to produce unsolvable mazes with unreachable sections, fixed now

--- src/Algorithms/huntAndKill.js
+++ src/Algorithms/huntAndKill.js
@@ -2,17 +2,18 @@ import { ROW, COL } from '../components/Grid/Grid'
 import {getNeighbors, removeWall, newGrid, getVisited} from './utility'
 
 /* preforms a random walk for the passed cell */
-function randomWalk(grid, gridArr, currCell){
+function randomWalk(grid, gridArr, currCell, remaining){
     while(true){
         currCell.current = true
         let neighbor = getNeighbors(grid, currCell)
         if(neighbor){
             removeWall(currCell, neighbor)
             neighbor.visited = true;
+            remaining -= 1
         } else {
             gridArr.push(newGrid(grid))
             currCell.current = false
-            return
+            return remaining
         }
 
         gridArr.push(newGrid(grid))
@@ -50,6 +51,63 @@ function hunt(grid){
 /* preforms the hunt and kill maze generation algorithm */
 export function huntAndKill(grid){
 
+
+    let gridArr = [] // used for visualization
+    let remaining = ROW * COL - 1 // remaining cells unvisited
+    console.log(remaining)
+    let currCell = grid[Math.floor(ROW/2)][Math.floor(COL/2)] // set the starting to the middle of the maze
+    currCell.visited = true // mark the current cell as visited
+    remaining = randomWalk(grid, gridArr, currCell, remaining) // preform an inital random walk and reassign remaining
+  
+    while(remaining > 0){
+        let stop = false
+        for(let row = 0; (row < ROW) && !stop; row++){
+            for(let col = 0; col < COL && !stop; col++){
+                let visitedNeighbors = getVisited(grid, grid[row][col])
+                let visitedNeighbor = visitedNeighbors[Math.floor(Math.random() * visitedNeighbors.length)];
+                if(grid[row][col].visited == false && visitedNeighbor){
+                    removeWall(grid[row][col], visitedNeighbor)
+                    grid[row][col].visited = true
+                    remaining -= 1
+                    remaining = randomWalk(grid, gridArr, grid[row][col], remaining)
+                    stop = true
+                }
+            }
+        }
+
+    }
+    
+    
+    // loop while there are still unvisited cells
+    /*
+    while(remaining > 0){
+        // loop through grid and look for unvisited cell with visited neighbor
+        
+    }
+
+    */
+
+
+
+    gridArr.push(newGrid(grid))
+    return gridArr
+
+    /*
+    for(let row = 0; row < ROW; row++){
+        for(let col = 0; col < COL; col++){
+            grid[row][col].path = true
+        }
+        gridArr.push(newGrid(grid))
+        for(let col = 0; col < COL; col++){
+            grid[row][col].path = false
+        }
+    }
+
+    gridArr.push(newGrid(grid))
+    return gridArr
+*/
+    /*
+
     let currCell = grid[Math.floor(ROW/2)][Math.floor(COL/2)]
     
     let gridArr = []
@@ -68,7 +126,7 @@ export function huntAndKill(grid){
     return gridArr
 
    
-    /*
+    
     for(let i = 0; i < grid.length; i++){
         for(let j = 0; j < grid[0].length; j++){
             if(grid[i][j].visited = false){
@@ -83,6 +141,8 @@ export function huntAndKill(grid){
         
 
         }
+        
+
         */
     
 }
